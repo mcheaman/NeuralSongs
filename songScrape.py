b04dd81574216401c05ff4ca14e39e292cb60ce7
Mason Heaman <mcheaman@yahoo.com>
2021-05-25 15:18:19 -0700

    corpus creation progress

--- songScrape.py
+++ songScrape.py
@@ -16,10 +16,10 @@ corpus = open("corpus.txt", "w", encoding='utf-8')
 genius = lg.Genius('D8JvvAIQsEfFTrpaKFQepQx2egIOJDTqtIXovRNmIerS-tPgE3A84vB717kbScDT', skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
 
 
-def get_songs(n):
+def get_songs(n, playlist_id):
     scope = "user-library-read"
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope),)
-    results = sp.playlist_items('37i9dQZF1DXaqCgtv7ZR3L?si=69d763c84af24554')
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    results = sp.playlist_items(playlist_id, limit=200)
     tracks = results['tracks']
     track_data = results['tracks']['items']
     song_dict = {}
@@ -39,20 +39,54 @@ def folder_corpus(songs):
                 break
         with open('corpus/'+'_'.join(song.split())+".txt", "w", encoding='utf-8') as f:
             print(genius.search_song(song, artist).lyrics, file=f)
-def compiled_corpus(songs):
+def compiled_corpus(songs, filename):
     lyrics = []
+    found  = 0
     for artist, song in songs.items():
         for k in range(0,len(song.split())):
             if '(' in song.split()[k] or '-' in song.split()[k]:
                 song = ' '.join(song.split()[:k])
                 break
-        lyrics.append(genius.search_song(song, artist).lyrics)
-    with open('corpus.txt', "w", encoding='utf-8') as f:
+        try:
+            lyrics.append(genius.search_song(song, artist).lyrics)
+            found +=1
+        except AttributeError:
+            print("Lyrics NOT found")
+        
+    with open(filename+'.txt', "w", encoding='utf-8') as f:
+        print('NUMBER OF SONGS FOUND' + str(found) + "\n", file =f)
         print('\n\n***************\n\n'.join(lyrics), file=f)
+        # print('\n\n'.join(lyrics), file=f)
+    return found
+
+top50_2020 = '37i9dQZF1DXaqCgtv7ZR3L?si=69d763c84af24554'
 
 
-songs = get_songs(50)
-folder_corpus(songs)
-compiled_corpus(songs)
+fifties = '37i9dQZF1DWSV3Tk4GO2fq?si=f8b1ed0c4b1241f0' # 150
+songs = get_songs(100, fifties)
+compiled_corpus(songs, '1950')
 
-# print(genius.search_song("Gangsta's Paradise", "Coolio").lyrics)
+sixties = '37i9dQZF1DXaKIA8E7WcJj?si=98b03662bfd84f1d' # 150
+songs = get_songs(100, sixties)
+compiled_corpus(songs, '1960')
+
+seventies = '37i9dQZF1DWTJ7xPn4vNaz?si=f690027939054ff1' # 150
+songs = get_songs(100, seventies)
+compiled_corpus(songs, '1970')
+
+eighties = '37i9dQZF1DX4UtSsGT1Sbe?si=72b043fb060d413f' # 150
+songs = get_songs(100, eighties)
+compiled_corpus(songs, '1980')
+
+nineties = '37i9dQZF1DXbTxeAdrVG2l?si=48292311eb264589' # 150
+songs = get_songs(100, nineties)
+compiled_corpus(songs, '1990')
+
+twothousands ='37i9dQZF1DX4o1oenSJRJd?si=6465ff05a30d459d' # 150
+songs = get_songs(100, twothousands)
+compiled_corpus(songs, '2000')
+
+todays_top50 = '37i9dQZF1DXcBWIGoYBM5M?si=d7e7ee5ee8bb416b'# 50
+songs = get_songs(50, todays_top50)
+compiled_corpus(songs, 'current50')
+# folder_corpus(songs)
