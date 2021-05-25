481a264a61b512bc192c82baaba0072602993fc5
Mason Heaman <mcheaman@yahoo.com>
2021-05-25 00:46:02 -0700

    modified to produce single&multi-file corpora

--- songScrape.py
+++ songScrape.py
@@ -12,57 +12,47 @@ import lyricsgenius as lg
 os.environ['SPOTIPY_CLIENT_ID'] = 'a613312a002b4e5baa4c2efaf91d20d6'
 os.environ['SPOTIPY_CLIENT_SECRET'] = 'abd026c24af64fb294849383b2dc6ce3'
 os.environ['SPOTIPY_REDIRECT_URI'] = 'https://example.com'
-corpus = open("corpus.txt", "w")
+corpus = open("corpus.txt", "w", encoding='utf-8')
 genius = lg.Genius('D8JvvAIQsEfFTrpaKFQepQx2egIOJDTqtIXovRNmIerS-tPgE3A84vB717kbScDT', skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
 
 
 def get_songs(n):
     scope = "user-library-read"
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope),)
-    results = sp.playlist_items('37i9dQZF1DXaqCgtv7ZR3L?si=69d763c84af24554', limit=5)
+    results = sp.playlist_items('37i9dQZF1DXaqCgtv7ZR3L?si=69d763c84af24554')
     tracks = results['tracks']
     track_data = results['tracks']['items']
     song_dict = {}
-    for i in range(10):
+    for i in range(n):
         song_dict[track_data[i]['track']['artists'][0]['name']] = track_data[i]['track']['name']
     return song_dict
 
-def generate_links(songs):
-    limit = len(songs);
-    links = {}
-    for artist, song in songs.items():
-        # print(artist + song)
-        name_split = artist.split()
-        for i in range(1, len(name_split)):
-            name_split[i] = name_split[i].lower()
-        artist_name = '-'.join(name_split)
-        song_split = song.split()
-        for i in range(0, len(song_split)):
-            # print(song_split[i])
-            if '(' in song_split[i]:
-                song_split = song_split[:i]
-                break
-            song_split[i] = song_split[i].lower()
-        song_name = '-'.join(song_split)
 
-        url ='https://genius.com/' + artist_name +'-'+ song_name + '-lyrics'
-        # print(url)
-        links[song] = url
-        limit -=1
-        if limit == 0:
-            break
-    return links
-
-songs = get_songs(10)
-links = generate_links(songs)
-# for song, link in links.items():
-#     print(song + ":\n" + link + '\n')
 
 # Scrape lyrics from a Genius.com song URL
-def scrape_song_lyrics(songs):
+def folder_corpus(songs):
+    # lyrics = []
+    for artist, song in songs.items():
+        for k in range(0,len(song.split())):
+            if '(' in song.split()[k] or '-' in song.split()[k]:
+                song = ' '.join(song.split()[:k])
+                break
+        with open('corpus/'+'_'.join(song.split())+".txt", "w", encoding='utf-8') as f:
+            print(genius.search_song(song, artist).lyrics, file=f)
+def compiled_corpus(songs):
     lyrics = []
     for artist, song in songs.items():
+        for k in range(0,len(song.split())):
+            if '(' in song.split()[k] or '-' in song.split()[k]:
+                song = ' '.join(song.split()[:k])
+                break
         lyrics.append(genius.search_song(song, artist).lyrics)
-    
+    with open('corpus.txt', "w", encoding='utf-8') as f:
+        print('\n\n***************\n\n'.join(lyrics), file=f)
 
-print(genius.search_song("The Box", "Roddy Ricch").lyrics)
+
+songs = get_songs(50)
+folder_corpus(songs)
+compiled_corpus(songs)
+
+# print(genius.search_song("Gangsta's Paradise", "Coolio").lyrics)
