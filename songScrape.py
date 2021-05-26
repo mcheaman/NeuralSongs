ce0535691e2b0baf48b02e61f5ef66795c193f91
Mason Heaman <mcheaman@yahoo.com>
2021-05-25 22:37:01 -0700

    Corpora by decade

--- songScrape.py
+++ songScrape.py
@@ -1,6 +1,7 @@
 # from PyLyrics.functions import PyLyrics
 from PyLyrics import *;
 import spotipy
+import credentials
 from spotipy.oauth2 import SpotifyOAuth
 import os
 import requests
@@ -9,22 +10,24 @@ import re
 import lyricsgenius as lg
 
 # Set environment variables
-os.environ['SPOTIPY_CLIENT_ID'] = 'a613312a002b4e5baa4c2efaf91d20d6'
-os.environ['SPOTIPY_CLIENT_SECRET'] = 'abd026c24af64fb294849383b2dc6ce3'
-os.environ['SPOTIPY_REDIRECT_URI'] = 'https://example.com'
+credentials.setEnvironment();
 corpus = open("corpus.txt", "w", encoding='utf-8')
-genius = lg.Genius('D8JvvAIQsEfFTrpaKFQepQx2egIOJDTqtIXovRNmIerS-tPgE3A84vB717kbScDT', skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
+genius = lg.Genius('D8JvvAIQsEfFTrpaKFQepQx2egIOJDTqtIXovRNmIerS-tPgE3A84vB717kbScDT', skip_non_songs=True, remove_section_headers=True)
 
 
 def get_songs(n, playlist_id):
     scope = "user-library-read"
+    tried = 0
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-    results = sp.playlist_items(playlist_id, limit=200)
+    results = sp.playlist_items(playlist_id)
     tracks = results['tracks']
     track_data = results['tracks']['items']
     song_dict = {}
     for i in range(n):
-        song_dict[track_data[i]['track']['artists'][0]['name']] = track_data[i]['track']['name']
+        tried +=1
+        song_dict[track_data[i]['track']['name']] = track_data[i]['track']['artists'][0]['name']
+    print('TRIED: ' + str(tried))
+    print('ACTUAL: ' + str(len(song_dict)))
     return song_dict
 
 
@@ -32,7 +35,7 @@ def get_songs(n, playlist_id):
 # Scrape lyrics from a Genius.com song URL
 def folder_corpus(songs):
     # lyrics = []
-    for artist, song in songs.items():
+    for song, artist in songs.items():
         for k in range(0,len(song.split())):
             if '(' in song.split()[k] or '-' in song.split()[k]:
                 song = ' '.join(song.split()[:k])
@@ -42,37 +45,39 @@ def folder_corpus(songs):
 def compiled_corpus(songs, filename):
     lyrics = []
     found  = 0
-    for artist, song in songs.items():
+    tried = 0
+    for song, artist in songs.items():
+        tried+=1
         for k in range(0,len(song.split())):
             if '(' in song.split()[k] or '-' in song.split()[k]:
                 song = ' '.join(song.split()[:k])
                 break
         try:
-            lyrics.append(genius.search_song(song, artist).lyrics)
+            lyrics.append(song.upper()+'\n\n'+genius.search_song(song, artist).lyrics)
             found +=1
         except AttributeError:
             print("Lyrics NOT found")
         
     with open(filename+'.txt', "w", encoding='utf-8') as f:
-        print('NUMBER OF SONGS FOUND' + str(found) + "\n", file =f)
-        print('\n\n***************\n\n'.join(lyrics), file=f)
+        print('NUMBER OF SONGS TRIED: ' + str(tried) + '\nNUMBER OF SONGS FOUND: ' + str(found) + "\n", file =f)
+        print('\n***************\n\n'.join(lyrics), file=f)
         # print('\n\n'.join(lyrics), file=f)
     return found
 
 top50_2020 = '37i9dQZF1DXaqCgtv7ZR3L?si=69d763c84af24554'
 
 
-fifties = '37i9dQZF1DWSV3Tk4GO2fq?si=f8b1ed0c4b1241f0' # 150
-songs = get_songs(100, fifties)
-compiled_corpus(songs, '1950')
+# fifties = '37i9dQZF1DWSV3Tk4GO2fq?si=f8b1ed0c4b1241f0' # 150
+# songs = get_songs(100, fifties)
+# compiled_corpus(songs, '1950')
 
-sixties = '37i9dQZF1DXaKIA8E7WcJj?si=98b03662bfd84f1d' # 150
-songs = get_songs(100, sixties)
-compiled_corpus(songs, '1960')
+# sixties = '37i9dQZF1DXaKIA8E7WcJj?si=98b03662bfd84f1d' # 150
+# songs = get_songs(100, sixties)
+# compiled_corpus(songs, '1960')
 
-seventies = '37i9dQZF1DWTJ7xPn4vNaz?si=f690027939054ff1' # 150
-songs = get_songs(100, seventies)
-compiled_corpus(songs, '1970')
+# seventies = '37i9dQZF1DWTJ7xPn4vNaz?si=f690027939054ff1' # 150
+# songs = get_songs(100, seventies)
+# compiled_corpus(songs, '1970')
 
 eighties = '37i9dQZF1DX4UtSsGT1Sbe?si=72b043fb060d413f' # 150
 songs = get_songs(100, eighties)
